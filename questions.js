d373e3d9050d55119141ddc8d9bf6cb92897c4b8
iammrmoo@gmail.com <iammrmoo@gmail.com>
2023-05-12 19:29:33 -0400

    added code to repository hopefully it works

---
+++ questions.js
@@ -0,0 +1,54 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Which one is a looping structure in JavaScript?",
+    choices: ["All the below", "For", "While", "do-while loops"],
+    answer: "All the below"
+  },
+  {
+    title: "What are the two basic groups of data types in JavaScript?",
+    choices: [
+      "Primitive and attribute",
+      "Primitive and reference types",
+      "Reference types and attribute",
+      "None of the above"
+    ],
+    answer: "Primitive and reference types"
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "Boolean operators that can be used in JavaScript include:",
+    choices: [
+      "'And' Operator &&",
+      "'Or' Operator ||",
+      "'Not' Operator !",
+      "All the above"
+    ],
+    answer: "All the above"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+];
